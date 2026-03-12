7686e642ed6a8e1a9564b1800adde9a50b814303
mqksrwq <maksim4ikb2006@gmail.com>
2026-03-12 09:43:48 +0300

    add registration

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -3,7 +3,7 @@ package apiserver
 type Config struct {
 	BindAddr    string `toml:"bind_addr"`
 	LogLevel    string `toml:"log_level"`
-	DataBaseURL string `toml:"data_base_url"`
+	DataBaseURL string `toml:"database_url"`
 }
 
 func NewConfig() *Config {
